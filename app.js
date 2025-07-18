6c0b5a273b9d44549ba6465dfcd72206879f2949
osa989 <osamagamal9988@gmail.com>
2025-07-18 04:00:11 +0300

    start

---
+++ app.js
@@ -0,0 +1,12 @@
+const anount =15 
+if (amount > 10) {
+    console.log("Amount is greater than 10");
+    
+}
+else {
+    console.log("Amount is 10 or less");
+}
+
+console.log(`hey it's me, the amount is ${amount}`);
+
+//to run the code just type node app.js in the terminal
